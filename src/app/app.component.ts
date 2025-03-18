0d1128dd1cf27a9000ee2c95c61a3cefc0131bbc
woratatk <woratatk@wisesoft.co.th>
2025-03-18 14:53:54 +0700

    add example for fflate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import JSZip from 'jszip';
-import { strToU8, zipSync } from 'fflate';
-import { ZipObject } from './model/model';
+import { zip, FlateError } from "fflate";
 import { RouterOutlet } from '@angular/router';
 import { ProgressbarModule } from 'ngx-bootstrap/progressbar';
 import { NgxSpinnerModule, NgxSpinnerService } from "ngx-spinner";
@@ -50,37 +49,82 @@ export class AppComponent {
 				const zipData = await response.blob();
 				mainZip.file(filename, zipData);
 	
-				// Update progress first 50% for fetching data
 				this.zipProgress = Math.round(((i + 1) / totalFiles) * 50);
 			}
 	
 			const zipBlob = await mainZip.generateAsync({ type: "blob" }, (metadata) => {
-				// Update progress second 50% for compression
 				this.zipProgress = 50 + Math.round(metadata.percent / 2);
 			});
 	
-			const link = document.createElement("a");
-			link.href = URL.createObjectURL(zipBlob);
-			link.download = "Main_Zip.zip";
-			link.click();
+			this.downloadFile(zipBlob);
 	
 			this.zipProgress = 100;
 	
 		} catch (error) {
 			console.error(error);
 		}
-	}	
+	}
 	
+	async zipMultipleZipFilesWithFflateDelay(): Promise<void> {
+		try {
+			const files: { [key: string]: Uint8Array } = {};
+			this.zipProgress = 0;
+			const totalFiles = 20;
+	
+			for (let i = 0; i < totalFiles; i++) {
+				const response = await this.getMockApiZipData();
+				const contentDisposition = response.headers.get("Content-Disposition");
+				const filename = contentDisposition ? this.extractFilenameFromContentDisposition(contentDisposition) : `file_${i + 1}.zip`;
+	
+				const zipData = new Uint8Array(await response.arrayBuffer());
+				files[filename] = zipData;
+	
+				this.zipProgress = Math.round(((i + 1) / totalFiles) * 50);
+			}
+	
+			let compressedSize = 0;
+			const fileEntries = Object.entries(files);
+			
+			const totalSize = fileEntries.reduce((prev, [_, data]) => prev + data.length, 0);
+	
+			const zipBlob = await new Promise<Blob>((resolve, reject) => {
+				const zipChunks: Uint8Array[] = [];
+	
+				zip(files, (err: FlateError | null, data: Uint8Array) => {
+					if (err) {
+						reject(err);
+						return;
+					}
+	
+					zipChunks.push(data);
+					compressedSize += data.length;
+	
+					this.zipProgress = 50 + Math.round((compressedSize / totalSize) * 50);
+	
+					if (compressedSize >= totalSize) {
+						resolve(new Blob(zipChunks, { type: "application/zip" }));
+					}
+				});
+			});
+	
+			this.downloadFile(zipBlob);
+	
+			this.zipProgress = 100;
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
 	private extractFilenameFromContentDisposition(contentDisposition: string): string {
-		const match = contentDisposition.match(/filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/);
+		const match = contentDisposition.match(/filename[^;=\n]*=(?:['"]?)([^'";\n]+)(?:['"]?)/);
 		return match ? match[1] : "default.zip";
 	}
 	
 	private async getMockApiZipData(): Promise<Response> {
-		const mockZipContent = new Uint8Array([80, 75, 3, 4, 20, 0, 0, 0, 8, 0]); // Mock ZIP file header
+		const mockZipContent = new Uint8Array([80, 75, 3, 4, 20, 0, 0, 0, 8, 0]);
 		const zipBlob = new Blob([mockZipContent], { type: "application/zip" });
 	
-		await new Promise(resolve => setTimeout(resolve, Math.random() * 1500));
+		await new Promise(resolve => setTimeout(resolve, Math.random() * 1000));
 	
 		return new Response(zipBlob, {
 			status: 200,
@@ -89,132 +133,13 @@ export class AppComponent {
 				"Content-Disposition": `attachment; filename="mock-file-${Math.random().toString(36).slice(2, 10)}.zip"`,
 			},
 		});
-	}	
-
-	async downloadNestedZip() {
-		const apiData = await this.getMockApiData();
-		await this.createNestedZipWithJsZip(apiData);
-		this.createNestedZipWithFflate(apiData);
 	}
 
-	async createNestedZipWithJsZip(apiData: any[]): Promise<void> {
-		const mainZip = new JSZip();
-
-		for (const contract of apiData) {
-			const subZip = new JSZip();
-
-			for (const file of contract.data) {
-				try {
-					subZip.file(file.filename, "Hello World\n");
-				} catch (error) {
-					console.error(`Failed to fetch fileId ${file.fileId}:`, error);
-				}
-			}
-
-			const subZipBlob = await subZip.generateAsync({ type: 'blob' });
-			mainZip.file(`${contract.contractNo}_${contract.contractSequence}.zip`, subZipBlob);
-		}
-
-		const mainZipBlob = await mainZip.generateAsync({ type: 'blob' });
-
-		const link = document.createElement('a');
-		link.href = URL.createObjectURL(mainZipBlob);
-		link.download = 'Main 1.zip';
+	private downloadFile(zipBlob: Blob) {
+		const link = document.createElement("a");
+		link.href = URL.createObjectURL(zipBlob);
+		link.download = "Main_Zip.zip";
 		link.click();
-	}
-
-	async createNestedZipWithFflate(apiData: any[]): Promise<void> {
-		const mainZip: { [key: string]: Uint8Array } = {};
-	
-		for (const contract of apiData) {
-			const subZip: { [key: string]: Uint8Array } = {};
-	
-			for (const file of contract.data) {
-				try {
-					subZip[file.filename] = strToU8("ทดสอบ Fflate\n");
-				} catch (error) {
-					console.error(`Failed to add file ${file.filename}:`, error);
-				}
-			}
-	
-			const subZipBlob = zipSync(subZip);
-	
-			const subZipFilename = `${contract.contractNo}_${contract.contractSequence}.zip`;
-			mainZip[subZipFilename] = subZipBlob;
-		}
-	
-		const mainZipBlob = zipSync(mainZip);
-
-		// compress each contract in parallel
-		// const mainZipEntries: { [key: string]: Uint8Array } = {};
-
-		// await Promise.all(
-		// 	apiData.map(async (contract) => {
-		// 		const subZipEntries: { [key: string]: Uint8Array } = {};
-		// 		contract.data.forEach((file: any) => {
-		// 			subZipEntries[file.filename] = strToU8(`ทดสอบไฟล์ที่ ${file.receiptId}`);
-		// 		});
-
-		// 		const subZipBlob = await new Promise<Uint8Array>((resolve, reject) => {
-		// 			zip(subZipEntries, {}, (err, data) => {
-		// 				if (err) {
-		// 					reject(err);
-		// 				} else {
-		// 					resolve(data);
-		// 				}
-		// 			});
-		// 		});
-
-		// 		mainZipEntries[`${contract.contractNo}_${contract.contractSequence}.zip`] = subZipBlob;
-		// 	})
-		// );
-
-		// const mainZipBlob2 = await new Promise<Uint8Array>((resolve, reject) => {
-		// 	zip(mainZipEntries, {}, (err, data) => {
-		// 		if (err) {
-		// 			reject(err);
-		// 		} else {
-		// 			resolve(data);
-		// 		}
-		// 	});
-		// });
-
-		const base64Zip = btoa(String.fromCharCode(...new Uint8Array(mainZipBlob.buffer)));
-
-		const downloadLink = document.createElement('a');
-		downloadLink.href = `data:application/zip;base64,${base64Zip}`;
-		downloadLink.download = 'Main 2.zip';
-		downloadLink.click();
-	}
-
-	private async getMockApiData(): Promise<any[]> {
-		return [
-			{
-				contractId: '1',
-				contractNo: 'C001',
-				contractSequence: '01',
-				data: [
-					{ receiptId: '1', receiptNo: 'P001', receiptSequence: '1', fileId: 'F001', filename: 'File1.txt', fileSize: 2048 },
-					{ receiptId: '2', receiptNo: 'P002', receiptSequence: '2', fileId: 'F002', filename: 'File2.txt', fileSize: 2048 },
-					{ receiptId: '3', receiptNo: 'P003', receiptSequence: '3', fileId: 'F003', filename: 'File3.txt', fileSize: 2048 },
-					{ receiptId: '4', receiptNo: 'P004', receiptSequence: '4', fileId: 'F004', filename: 'File4.txt', fileSize: 2048 },
-					{ receiptId: '5', receiptNo: 'P005', receiptSequence: '5', fileId: 'F005', filename: 'File5.txt', fileSize: 2048 },
-					{ receiptId: '6', receiptNo: 'P006', receiptSequence: '6', fileId: 'F006', filename: 'File6.txt', fileSize: 2048 },
-					{ receiptId: '7', receiptNo: 'P007', receiptSequence: '7', fileId: 'F007', filename: 'File7.txt', fileSize: 2048 },
-					{ receiptId: '8', receiptNo: 'P008', receiptSequence: '8', fileId: 'F008', filename: 'File8.txt', fileSize: 2048 },
-					{ receiptId: '9', receiptNo: 'P009', receiptSequence: '9', fileId: 'F009', filename: 'File9.txt', fileSize: 2048 },
-					{ receiptId: '10', receiptNo: 'P010', receiptSequence: '10', fileId: 'F010', filename: 'File10.txt', fileSize: 2048 },
-				],
-			},
-			{
-				contractId: '2',
-				contractNo: 'C002',
-				contractSequence: '02',
-				data: [
-					{ receiptId: '11', receiptNo: 'P011', receiptSequence: '1', fileId: 'F011', filename: 'File11.txt', fileSize: 2048 },
-				],
-			},
-		];
 	}
 	
 }
